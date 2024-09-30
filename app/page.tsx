38a2273e4c90e17a121b77acb5a1c7b06e362020
Ayub Abdi <ayubabdi26@gmail.com>
2024-09-30 22:33:30 +1300

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,7 @@ const LandingPage = () => {
                 <input
                   type="email"
                   placeholder="Enter your email address"
-                  className="px-4 py-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none bg-white-800 text-white w-full"
+                  className="px-4 py-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none bg-white-800 text-black w-full"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   required
