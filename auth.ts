c5ccf3fdd187b0ac1f7fb05b5532f13ec0f576a6
Ayub Abdi <ayubabdi26@gmail.com>
2024-11-27 00:01:29 +1300

    test

--- auth.ts
+++ auth.ts
@@ -4,6 +4,18 @@ import Resend from "next-auth/providers/resend"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/app/lib/prisma"
 import { createStripeAccount } from "@/app/lib/actions"
+
+const handleLogin = async ({ user, account }: { user: any; account: any }) => {
+  if (user && account) {
+    try {
+      await createStripeAccount(user.id as string);
+    } catch (error) {
+      console.error("Error processing login:", error);
+      return false; // Prevent login if server action fails
+    }
+  }
+  return true;
+};
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
@@ -18,17 +30,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
       }}), Resend({from: "no-reply@skillwave.io"})],
   callbacks: {
-    async signIn({ user, account }) {
-      // This is where you can trigger your server action
-      if (user && account) {
-        try {
-          await createStripeAccount(user.id as string)
-        } catch (error) {
-          console.error("Error processing login:", error)
-          return false // Prevent login if server action fails
-        }
-      }
-      return true
+    async signIn(args) {
+      return handleLogin(args)
     }
   }
 })  
