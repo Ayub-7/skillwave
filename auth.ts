3aff65c8930b1130d9d5e09003c0c248c5c02b51
Ayub Abdi <ayubabdi26@gmail.com>
2024-11-26 22:57:24 +1300

    test

--- auth.ts
+++ auth.ts
@@ -6,41 +6,41 @@ import prisma from "@/app/lib/prisma"
 import { stripe } from "@/app/lib/stripe"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  callbacks: {
-    async signIn({ account, profile }) {
-      let dbUser = await prisma.user.findUnique({
-        where: {
-          id: account?.userId,
-        },
-      });
+  // callbacks: {
+  //   async signIn({ account, profile }) {
+  //     let dbUser = await prisma.user.findUnique({
+  //       where: {
+  //         id: account?.userId,
+  //       },
+  //     });
 
-      if (!dbUser) {
-        const stripeAccount = await stripe.accounts.create({
-          email: profile?.email as string,
-          controller: {
-            losses: {
-              payments: "application",
-            },
-            fees: {
-              payer: "application",
-            },
-            stripe_dashboard: {
-              type: "express",
-            },
-          },
-        });
-        await prisma.user.update({
-          where: {
-            id: account?.userId
-          },
-          data: {
-            connectedAccountId: stripeAccount.id,
-          },
-        });
-      }
-      return true
-    },
-  },
+  //     if (!dbUser) {
+  //       const stripeAccount = await stripe.accounts.create({
+  //         email: profile?.email as string,
+  //         controller: {
+  //           losses: {
+  //             payments: "application",
+  //           },
+  //           fees: {
+  //             payer: "application",
+  //           },
+  //           stripe_dashboard: {
+  //             type: "express",
+  //           },
+  //         },
+  //       });
+  //       await prisma.user.update({
+  //         where: {
+  //           id: account?.userId
+  //         },
+  //         data: {
+  //           connectedAccountId: stripeAccount.id,
+  //         },
+  //       });
+  //     }
+  //     return true
+  //   },
+  // },
   pages: {
     verifyRequest: '/auth/verify-request',
   },
