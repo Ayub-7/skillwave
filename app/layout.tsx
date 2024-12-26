77464bb717868623ed659a32229bb37c9b84a3b2
Ayub Abdi <ayubabdi26@gmail.com>
2024-12-26 15:07:50 +1300

    Add referral program

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { karla } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import Providers from './providers';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
@@ -30,6 +31,7 @@ export default function RootLayout({
           }}
         />
         <Providers>
+          <Script async src="https://cdn.promotekit.com/promotekit.js" data-promotekit="7c7b82ab-2a76-4da0-8a60-fae6713dad6c"></Script>
           {children}
           <Analytics />
         </Providers>
