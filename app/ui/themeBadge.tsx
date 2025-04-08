9b954968f36be2cb5511b4e5be63fb848c70e062
ayub-7 <ayubabdi26@gmail.com>
2025-04-09 01:11:34 +1200

    add tiny back

--- app/ui/themeBadge.tsx
+++ app/ui/themeBadge.tsx
@@ -7,16 +7,19 @@ import TinyLaunchBadgeDark from "@/public/tinylaunchDark";
 export default function ThemeBadge() {
     const { theme } = useTheme();
 
-    const imageStyle = {
-        width: '225px', height: '54px',
-    }
-
     return (
+        <a
+            href="https://tinylaun.ch"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:scale-105 transition-transform"
+        >
+            {theme === "dark" ? (
+                <TinyLaunchBadgeDark className="h-12 w-auto" />
 
-        theme === "dark" ? (
-            <a className="hover:scale-105 transition-transform" href="https://www.producthunt.com/posts/skillwave-io?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-skillwave&#0045;io" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=852804&theme=dark&t=1740132937132" alt="skillwave&#0046;io - The&#0032;modern&#0032;platform&#0032;for&#0032;course&#0032;creators | Product Hunt" style={imageStyle} width="250" height="54" /></a>
-        ) : (
-            <a className="hover:scale-105 transition-transform" href="https://www.producthunt.com/posts/skillwave-io?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-skillwave&#0045;io" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=852804&theme=light&t=1740133242401" alt="skillwave&#0046;io - The&#0032;modern&#0032;platform&#0032;for&#0032;course&#0032;creators | Product Hunt" style={imageStyle} width="250" height="54" /></a>
-        )
+            ) : (
+                <TinyLaunchBadge className="h-12 w-auto" />
+            )}
+        </a>
     );
 }
